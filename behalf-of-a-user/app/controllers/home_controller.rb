1b262cfc9feae949a0f2f6b05e4635649c988f99
thaim <thaim24@gmail.com>
2023-10-09 02:05:20 +0900

    add link login with github

--- behalf-of-a-user/app/controllers/home_controller.rb
+++ behalf-of-a-user/app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @github_login_path = github_login_path
+  end
+
+  def github_login_path
+    params = {
+        "client_id" => ENV["GITHUB_CLIENT_ID"]
+    }
+    uri = URI("https://github.com/login/oauth/authorize")
+    uri.query = params.to_query
+
+    uri.to_s
   end
 end
