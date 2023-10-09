cb58b2c1c9c7c3ecec7802e39830f7e69f093736
thaim <thaim24@gmail.com>
2023-10-09 13:50:40 +0900

    create session with github login callback

--- behalf-of-a-user/app/controllers/sessions_controller.rb
+++ behalf-of-a-user/app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
   def create
+    code = params["code"]
+
+    redirect_to home_index_path, notice: 'ログインしました'
   end
 end
--- behalf-of-a-user/config/routes.rb
+++ behalf-of-a-user/config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get '/github/callback', to: 'sessions#create'
 end
